6afb199f88a6def161725f334882e0d5d255b874
Manuel-hub-s <83520897+Manuel-hub-s@users.noreply.github.com>
2021-05-27 13:31:51 -0600

    Update Dat Hidro R.R

--- Dat Hidro R.R
+++ Dat Hidro R.R
@@ -1,9 +1,9 @@
 #datos hidrológicos Tarea II
 
-inp <- read.csv("FDC.csv", na.strings ="")
+inp <- read.csv("FDC.csv", na.strings ="") En esta seccion se busca adjuntar e importar los datos hidrologicos a la plataforma o servidor R. 
 
-head(inp)
-dim(inp)
+head(inp) En esta parte se pide como el limite maximo y el minimo de toda la lista de datos
+dim(inp) Reporta la dimension del data frame (en este caso cantidad de datos, 3846 de dos columnas)
 
 
 inp[!complete.cases(inp),]
@@ -12,21 +12,22 @@ inp[!complete.cases(inp),]
 
 plot(inp[,2], type = "l", col="blue")
 lines(inp[,3], col="green")
-
-summary(inp[,2:3])
-hist(inp[,2])
-hist(inp[,3])
+En esta seccion nos grafica los datos de una y la otra columna cada una de un color durante los años o el periodo de tiempo indicado 
+summary(inp[,2:3]) resume 
+hist(inp[,2]) Nos hace un histograma de los datos para Pandora
+hist(inp[,3]) Nos hace un histograma de los datos para Banano
 
 names(inp) <- c("fecha", "Pandora", "Banano")
 attach(inp)
 plot(Pandora)
 
 Template <- strptime(inp[,1], format= "%d/%m/%Y")
+Provee con las fechas el registro de cada dato ordenado segun su antiguedad
 
-MAQ_Pandora <- tapply(Pandora, format(Template, format="%Y"), FUN=sum)
+MAQ_Pandora <- tapply(Pandora, format(Template, format="%Y"), FUN=sum) Le da formato de lectura en tabla y suma los datos segun cada año para cada columna segun rio Estrella o como se guardo Pandora y el rio banano
 MAQ_Banano <- tapply(Banano, format(Template, format="%Y"), FUN=sum)
 
-write.csv(rbind(MAQ_Pandora,MAQ_Banano), file="MAQ.csv")
+write.csv(rbind(MAQ_Pandora,MAQ_Banano), file="MAQ.csv") 
 
 plot(MAQ_Banano, ylim=c(100,3000))
 lines(MAQ_Pandora, col=2)
@@ -35,12 +36,12 @@ MMQ_Pandora <- tapply(Pandora, format(Template, format="%m"), FUN=sum)
 MMQ_Banano <- tapply(Banano, format(Template, format="%m"), FUN=sum)
 
 # Análisis de correlacion
-corinp <- cor(inp[,2:3],method = "spearman")
+corinp <- cor(inp[,2:3],method = "spearman") 
 
 
 
 inp.lm <- lm(inp[,2] ~ inp[,3], data=inp)
 summary(inp.lm)
-plot(inp.lm)
+plot(inp.lm) Buscamos un patron por lo cual el plot establece un promedio de los datos generados al final de las sumas y dibuja una linea que define ese patron   
 
 
